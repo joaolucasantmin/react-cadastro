aae926132d93cd6719851a277abb86c3dab900f8
joaolucasantmin <joaolucasantmin@gmail.com>
2026-08-07 14:36:32 -0300

    App.jsx voltei para o original, mas ainda nao esta funcionando a hospedagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import Cadastro from "./pages/Pages/Cadastro"
-//import Login from "./pages/Pages/Login"
+import Login from "./pages/Pages/Login"
 import TelaMain from "./pages/Pages/telaMain"
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<h1>TESTE CADASTRO</h1>} />
+        <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/home" element={<TelaMain />} />
       </Routes>
     </BrowserRouter>
